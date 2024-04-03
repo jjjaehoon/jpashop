8bda5b3641986260491eb2e553e42c4a502cdeb4
jjh <jjjaehoon@naver.com>
2024-04-03 18:55:58 +0900

    실전! 스프링 부트와 JPA 활용1 - 도메인 분석 설계 2 - Address, Item Entity 수정 - Category Entity 추가

---
+++ src/main/java/com/jpabook/jpashop/domain/Category.java
@@ -0,0 +1,44 @@
+package com.jpabook.jpashop.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.jpabook.jpashop.domain.item.Item;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Category {
+
+	@Id
+	@GeneratedValue
+	@Column(name = "category_id")
+	private Long id;
+
+	private String name;
+
+	@ManyToMany
+	@JoinTable(name = "category_item",
+		joinColumns = @JoinColumn(name = "category_id"),
+		inverseJoinColumns = @JoinColumn(name = "item_id"))
+	private List<Item> items = new ArrayList<>();
+
+	@ManyToOne
+	@JoinColumn(name = "parent_id")
+	private Category parent;
+
+	@OneToMany(mappedBy = "parent")
+	private List<Category> child = new ArrayList<>();
+}
--- src/main/java/com/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/com/jpabook/jpashop/domain/item/Item.java
@@ -1,5 +1,10 @@
 package com.jpabook.jpashop.domain.item;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.jpabook.jpashop.domain.Category;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.DiscriminatorColumn;
 import jakarta.persistence.Entity;
@@ -7,6 +12,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Inheritance;
 import jakarta.persistence.InheritanceType;
+import jakarta.persistence.ManyToMany;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -24,4 +30,7 @@ public abstract class Item {
 	private String name;
 	private int price;
 	private int stockQuantity;
+
+	@ManyToMany(mappedBy = "items")
+	private List<Category> categories = new ArrayList<>();
 }
