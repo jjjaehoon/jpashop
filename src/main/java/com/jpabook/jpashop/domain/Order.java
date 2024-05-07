2f08b6cf11b4ddf4d9ddfa4584e74bef30f55f36
jjh <jjjaehoon@naver.com>
2024-05-07 16:39:03 +0900

    실전! 스프링 부트와 JPA 활용1 - 도메인 분석 설계 4 - 특정 엔티티에 연관관계 메서드 추가

--- src/main/java/com/jpabook/jpashop/domain/Order.java
+++ src/main/java/com/jpabook/jpashop/domain/Order.java
@@ -6,6 +6,7 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -34,7 +35,7 @@ public class Order {
 	@JoinColumn(name = "member_id")
 	private Member member;
 
-	@OneToMany(mappedBy = "order")
+	@OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
 	private List<OrderItem> orderItems = new ArrayList<>();
 
 	@OneToOne(fetch = LAZY)
@@ -46,5 +47,19 @@ public class Order {
 	@Enumerated(EnumType.STRING)
 	private OrderStatus status; //주문상태 [ORDER, CANCEL]
 
+	//연관관계 메서드
+	/public void setMember(Member member) {
+		this.member = member;
+		member.getOrders().add(this);
+	}
 
+	public void addOrderItem(OrderItem orderItem) {
+		orderItems.add(orderItem);
+		orderItem.setOrder(this);
+	}
+
+	public void setDelivery(Delivery delivery) {
+		this.delivery = delivery;
+		delivery.setOrder(this);
+	}
 }
--- src/main/java/com/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/com/jpabook/jpashop/domain/OrderItem.java
@@ -28,11 +28,11 @@ public class OrderItem {
 	@JoinColumn(name = "item_id")
 	private Item item;
 
-
 	@ManyToOne(fetch = LAZY)
 	@JoinColumn(name = "order_id")
 	private Order order;
 
 	private int orderPrice;	//주문 가격
 	private int count; //주문 수량
+
 }
